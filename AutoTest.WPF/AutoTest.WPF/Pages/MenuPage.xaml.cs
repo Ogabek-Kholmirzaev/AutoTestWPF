ee7dddc9c5e42a40a7509035164b12a5b0122501
Ogabek <xolmirzayev.ogabek.2004@gmail.com>
2022-09-16 09:02:22 +0500

    Autotest.WPF

---
+++ AutoTest.WPF/AutoTest.WPF/Pages/MenuPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoTest.WPF.Pages
+{
+    /// <summary>
+    /// Interaction logic for MenuPage.xaml
+    /// </summary>
+    public partial class MenuPage : Page
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TicketsButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow.InstanceMainWindow.MainWindowFrame.Navigate(new TicketsPage());
+        }
+        private void ExaminationButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow.InstanceMainWindow.MainWindowFrame.Navigate(new ExaminationPage());
+        }
+    }
+}
---
+++ AutoTest.WPF/AutoTest.WPF/Pages/TicketsPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoTest.WPF.Pages
+{
+    /// <summary>
+    /// Interaction logic for TicketsPage.xaml
+    /// </summary>
+    public partial class TicketsPage : Page
+    {
+        public TicketsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuButtonClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow.InstanceMainWindow.MainWindowFrame.Navigate(new MenuPage());
+        }
+    }
+}
